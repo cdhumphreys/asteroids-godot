baf5336a74356d01fddec9180458599c06548626
Carl humphreys <humphreys.carl.d@gmail.com>
2025-11-17 16:46:17 +0000

    splitting asteroids on hit

--- scripts/asteroid.gd
+++ scripts/asteroid.gd
@@ -11,6 +11,9 @@ var movement_vector: Vector2 = Vector2(0, -1)
 
 @export var stats: AsteroidStats
 
+var small_asteroid_scene: PackedScene = preload("res://scenes/asteroid.tscn")
+var small_asteroid_stat: AsteroidStats = preload("res://resources/asteroids/small_asteroid.tres")
+
 func _ready() -> void:
 	speed = randf_range(stats.MIN_SPEED, stats.MAX_SPEED)
 	rotation = randf_range(0, 2*PI)
@@ -33,3 +36,26 @@ func _physics_process(delta: float) -> void:
 
 	global_position += movement_vector.rotated(rotation) * speed * delta
 	position = Utils.keep_body_in_screen_bounds(global_position, get_viewport_rect(), width, height)
+
+
+func _on_hit_by_bullet():
+	if stats.size == Enums.AsteroidSize.SMALL:
+		queue_free()
+	elif stats.size == Enums.AsteroidSize.LARGE:
+		_split_into_smaller()
+		queue_free()
+
+func _split_into_smaller():
+	var root_node = get_tree().root
+	for i in range(2):
+		var new_asteroid: Asteroid = small_asteroid_scene.instantiate()
+		new_asteroid.stats = small_asteroid_stat
+		root_node.add_child(new_asteroid)
+		new_asteroid.global_position = global_position
+	
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Bullet:
+		area.queue_free()
+		_on_hit_by_bullet()
+		
