4a7aba91d5bc64142ea301f95b8a82e5ac83c8c4
Carl Humphreys <carlh.crimson@gmail.com>
2025-12-16 19:02:20 +0000

    Adding pause menu open/close sounds

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,18 +1,31 @@
 extends CanvasLayer
 
 class_name PauseMenu
-@onready var popup_handler: PopupHandler = %PopupHandler
 
+@onready var popup_handler: PopupHandler = %PopupHandler
 @onready var continue_button: Button = %ContinueButton
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+# DOOR_OPEN_002
+const OPEN_SOUND = preload("uid://cl4e6fube4len")
+# DOOR_CLOSE_002
+const CLOSE_SOUND = preload("uid://b3ntcp4qu8uor")
 
 
 func on_show():
 	popup_handler.focus_first_element()
+	
+	audio_stream_player.stream = OPEN_SOUND
+	audio_stream_player.play()
 
+func on_hide():
+	audio_stream_player.stream = CLOSE_SOUND
+	audio_stream_player.play()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_continue_button_pressed() -> void:
+	on_hide()
 	EventBus.continue_button_pressed.emit()
